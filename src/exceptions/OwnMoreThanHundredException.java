a24752062b213e5c2b30d5b8f410eb0894d39d33
Гаврилюк Олександр Михайлович <o.gavrilyuk@wr.int>
2019-07-16 10:34:12 +0300

    Rebuild project, analyze and optimize code

--- src/exceptions/OwnMoreThanHundredException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class OwnMoreThanHundredException extends Exception{
-    public OwnMoreThanHundredException (String message){
-        super(message);
-    }
-}
--- src/main/java/DataServer.java
+++ src/main/java/DataServer.java
@@ -1,13 +1,18 @@
-import exceptions.OwnMoreThanHundredException;
-import exceptions.OwnNullObjectException;
-import exceptions.OwnOddException;
-import exceptions.OwnPalindromeException;
+package main.java;
 
-import java.util.*;
+import main.java.exceptions.OwnMoreThanHundredException;
+import main.java.exceptions.OwnNullObjectException;
+import main.java.exceptions.OwnOddException;
+import main.java.exceptions.OwnPalindromeException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 class DataServer {
 
-    public static void checkPalindrome(String s) throws OwnPalindromeException {
+    static void checkPalindrome(String s) throws OwnPalindromeException {
         if (s.equals(new StringBuilder(s).reverse().toString())) {
             System.out.println("Word" + s + " is a palindrome");
         } else {
@@ -15,7 +20,7 @@ class DataServer {
         }
     }
 
-    public static void checkNumber(Integer n) throws OwnMoreThanHundredException, OwnOddException {
+    static void checkNumber(Integer n) throws OwnMoreThanHundredException, OwnOddException {
         if (n > 100) {
             throw new OwnMoreThanHundredException("Entered number is greater than 100!");
         } else if (n % 2 == 0) {
@@ -25,15 +30,14 @@ class DataServer {
         }
     }
 
-    public static String checkObject(Object object) throws OwnNullObjectException {
+    static void checkObject(Object object) throws OwnNullObjectException {
         if (Optional.ofNullable(object).isEmpty()) {
             throw new OwnNullObjectException("The object is null");
         } else {
-            return ("Entered object isn't null, object class is: " + object.getClass());
         }
     }
 
-    public static void stackOverflowError(int i) {
+    static void stackOverflowError(int i) {
         System.out.println("Number: " + i);
         if (i == 0)
             System.out.println("Good choice! No stackOverflowError!");
@@ -41,13 +45,14 @@ class DataServer {
             stackOverflowError(++i);
     }
 
-    public static void outOfMemoryError (String shure){
-        if (shure.equals("Yes")){List<Integer> list = new ArrayList<>();
-        Random random = new Random();
-        while (true)
-            list.add(random.nextInt());}
-        else if (shure.equals("No")) System.out.println("Good choice! No outOfMemoryError");
-        else{
+    static void outOfMemoryError(String shure) {
+        if (shure.equals("Yes")) {
+            List<Integer> list = new ArrayList<>();
+            Random random = new Random();
+            while (true)
+                list.add(random.nextInt());
+        } else if (shure.equals("No")) System.out.println("Good choice! No outOfMemoryError");
+        else {
             System.out.println("Enter your answer again ('Yes' or 'No')");
         }
     }
